4aff4283bafbeaca37c0c4976364af49c0029d7f
Raúl Contreras <33237648+RaulContrerasKYA@users.noreply.github.com>
2020-10-31 00:35:38 -0600

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -2,7 +2,7 @@ def main():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
-  #quotes = f.readlines()
+  #quotes = f.readlines() Esto es realmente extraño no acabo de entender
   #f.close() Tengo que ver porque no se guardan los cambios! esto es raro!
 
   #print(quotes)
